dd36cda8561c9151cc3298218c92a655e399d3aa
perezalexreal <perezalexreal@gmail.com>
2022-03-11 16:24:45 +0100

    Laravel UI Installato e Bootstrap. Manca solo l'aggiunta di Vue

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/admin', 'App\Http\Controllers\Admin\HomeController@index')->name('admin.home');
